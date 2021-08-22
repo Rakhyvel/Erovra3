ce1396950f7744653af4457399975e8856415596
Rakhyvel <josephmshimel@gmail.com>
2021-08-21 20:48:27 -0700

    Commentate

--- Erovra3/src/scenes/pause.c
+++ Erovra3/src/scenes/pause.c
@@ -7,45 +7,76 @@
 #include "./match.h"
 #include "./menu.h"
 
+/* GUI containers */
 EntityID pauseMenuContainer;
 EntityID victoryMenuContainer;
 EntityID defeatMenuContainer;
 
+/* GUI images for splash */
 EntityID victoryImage;
 EntityID defeatImage;
-
 SDL_Texture* victoryTexture = NULL;
 SDL_Texture* defeatTexture = NULL;
 
+/* The state the pause menu is currently in */
 enum pauseState state;
 
+/* Reference to the match scene. Used to render match in background */
 static Scene* matchScene = NULL;
+
+/* Transition value */
 int fade = 0;
 
+/*	Called from pause menu. Ends game in defeat.
+ * 
+ *	@param scene	Scene reference (unused)
+ *	@param id		EntityID of button that trigged callback (unused)
+ */
 void Pause_Surrender(Scene* scene, EntityID id)
 {
     state = DEFEAT;
 }
 
+/*	Called from pause menu. Returns to match
+ * 
+ *	@param scene	Scene reference (unused)
+ *	@param id		EntityID of button that trigged callback (unused)
+ */
 void Pause_BackToGame(Scene* scene, EntityID id)
 {
     state = RETURN_MATCH;
 }
 
+/*	Called from surrender or victory menus. Returns to main menu
+ * 
+ *	@param scene	Scene reference (unused)
+ *	@param id		EntityID of button that trigged callback (unused)
+ */
 void Pause_BackToMenu(Scene* scene, EntityID id)
 {
     state = RETURN_MENU;
 }
 
+/*	Called from surrender or victory menus. Ends program.
+ * 
+ *	@param scene	Scene reference (unused)
+ *	@param id		EntityID of button that trigged callback (unused)
+ */
 void Pause_Exit(Scene* scene, EntityID id)
 {
     exit(0);
 }
 
+/*	Checks menu state, and increments/decrements fade transition value accordingly
+ * 
+ *	@param scene	Scene reference
+ */
 void Pause_Update(Scene* scene)
 {
     static escDown = true;
+	// Regular menus
     if (state == PAUSE || state == VICTORY || state == DEFEAT) {
+		// If in pause scene, check if esc key is pressed. Return to match if so
         if (state == PAUSE && g->keys[SDL_SCANCODE_ESCAPE]) {
             if (!escDown) {
                 state = RETURN_MATCH;
@@ -56,12 +87,18 @@ void Pause_Update(Scene* scene)
         } else {
             escDown = false;
         }
+
+		// Update transition value
         if (fade < 128) {
             fade += 128 / 10;
         }
-    } else if (fade > 0) {
+    } 
+	// Transition states RETURN_MATCH and RETURN_MENU. Update transistion states
+	else if (fade > 0) {
         fade -= 128 / 10;
-    } else {
+    } 
+	// Finally execute actions
+	else {
         // Reset statics to default before leaving
         escDown = true;
         if (state == RETURN_MATCH) {
@@ -72,12 +109,14 @@ void Pause_Update(Scene* scene)
         return;
     }
 
+	// Translate containers and images according to fade value
     GUI_CenterElementAt(scene, pauseMenuContainer, 0, pow((128.0f - fade) / 128.0f, 2) * 1000);
     GUI_CenterElementAt(scene, victoryMenuContainer, 0, pow((128.0f - fade) / 128.0f, 2) * 1000);
     GUI_CenterElementAt(scene, defeatMenuContainer, 0, pow((128.0f - fade) / 128.0f, 2) * 1000);
     GUI_CenterElementAt(scene, victoryImage, 0, -150 - pow((128.0f - fade) / 128.0f, 2) * 1000);
     GUI_CenterElementAt(scene, defeatImage, 0, -150 - pow((128.0f - fade) / 128.0f, 2) * 1000);
 
+	// Select which container and image to show based on state
     GUI_SetShown(scene, pauseMenuContainer, state == PAUSE);
     GUI_SetShown(scene, victoryMenuContainer, state == VICTORY);
     GUI_SetShown(scene, victoryImage, state == VICTORY);
@@ -87,6 +126,11 @@ void Pause_Update(Scene* scene)
     GUI_Update(scene);
 }
 
+/*	Calls match render system, with a translucent overlay accoridng to the fade,
+ *	and finally draws the menus. 
+ * 
+ *	@param scene	Scene reference
+ */
 void Pause_Render(Scene* scene)
 {
     Match_Render(matchScene);
@@ -95,11 +139,20 @@ void Pause_Render(Scene* scene)
     GUI_Render(scene);
 }
 
+/*	Destroys scene
+ * 
+ *	@param scene	Scene reference to destroy
+ */
 void Pause_Destroy(Scene* scene)
 {
     GUI_Destroy(scene);
 }
 
+/*	Creates scene
+ * 
+ *	@param scene	Scene reference
+ *	@param s		Pause state to start pause scene in
+ */
 Scene* Pause_Init(Scene* mScene, enum pauseState s)
 {
     Scene* scene = Scene_Create(&GUI_Register, &Pause_Update, &Pause_Render, &Pause_Destroy);
